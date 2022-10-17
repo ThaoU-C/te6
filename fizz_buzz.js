f1a1a354c83017059e526757924956bf4a41bd95
ThaoU-C <thaouyenula@gmail.com>
2022-10-16 23:05:45 -0700

    TE6 Finished

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,6 +1,21 @@
-for (let number = 1; number <= 100; number++) {
 
-  console.log(number);
+for ( var number = 1; number <= 100; number ++ ) {
+  
+  var diviby3 = number % 3;
+  var diviby5 = number % 5;
 
-
-}
+  // FizzBuzz
+  if ( (diviby3 == 0) && (diviby5 == 0) ) 
+  	console.log( "FizzBuzz");
+  
+  //Fizz
+  else if (diviby3 == 0)
+    console.log( "Fizz");
+  
+  //Buzz
+  else if (diviby5 == 0)
+    console.log( "Buzz");
+  
+  else
+    console.log( number );
+} 
